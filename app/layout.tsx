0bc606bdde413df683d0d0911389becbefd1159c
Dipankar Bhatia <dipankar@Dipankars-MacBook-Air.local>
2025-01-20 16:10:06 -0500

    Capitalized title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: "🌟 Dipankar's portfolio website",
-  description: "Showcasing Dipankar's expertise as a software engineer, featuring projects, skills, and experience in creating impactful solutions.",
+  title: "🌟 Dipankar's Portfolio Website",
+  description: "Showcasing my expertise as a software engineer, featuring projects, skills, and experience in creating impactful solutions.",
 }
 
 export default function RootLayout({
